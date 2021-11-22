7828da34a41f1d043ec8378afcc11bdf005092db
Kyle <kliang1194@gmail.com>
2021-11-22 16:01:12 -0500

    Add BootcampX Queries 4

---
+++ 4_queries/13_names_of_teachers_and_numbers_of_assistances.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+FROM cohorts
+JOIN students ON cohort_id = cohorts.id
+JOIN assistance_requests ON student_id = students.id
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
